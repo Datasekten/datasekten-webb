6254d6ba976bca863a79e8ef9a946516aacac6a9
Qinghuqu <qinghuqu@gmail.com>
2021-04-06 14:25:43 +0200

    Remove class mainText and new indentation

--- skript.js
+++ skript.js
@@ -1,11 +1,11 @@
 var navLinks = document.getElementsByClassName("navLink");
 var linksCount = 0;
 while (linksCount < navLinks.length) {
-    navLinks[linksCount].addEventListener("mouseover", function(){
-        this.innerHTML = "&gt" + this.innerHTML + "&lt";
-    });
-    navLinks[linksCount].addEventListener("mouseout", function(){
-        this.innerHTML = this.innerHTML.substring(4, this.innerHTML.length-4);
-    });
-    linksCount += 1;
+  navLinks[linksCount].addEventListener("mouseover", function(){
+    this.innerHTML = "&gt" + this.innerHTML + "&lt";
+  });
+  navLinks[linksCount].addEventListener("mouseout", function(){
+    this.innerHTML = this.innerHTML.substring(4, this.innerHTML.length-4);
+  });
+  linksCount += 1;
 }
