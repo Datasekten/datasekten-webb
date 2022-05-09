259cb5b4c8d65209c59fd048d9eea09aab0d33e5
qinghuqu <qinghuqu@gmail.com>
2022-05-09 14:56:35 +0200

    Reformat js

--- skript.js
+++ skript.js
@@ -1,11 +1,18 @@
 var navLinks = document.getElementsByClassName("navLink");
 var linksCount = 0;
+
 while (linksCount < navLinks.length) {
-  navLinks[linksCount].addEventListener("mouseover", function(){
+
+  navLinks[linksCount].addEventListener("mouseover", function()
+  {
     this.innerHTML = "&gt" + this.innerHTML + "&lt";
   });
-  navLinks[linksCount].addEventListener("mouseout", function(){
+
+  navLinks[linksCount].addEventListener("mouseout", function()
+  {
     this.innerHTML = this.innerHTML.substring(4, this.innerHTML.length-4);
   });
+
   linksCount += 1;
+  
 }
